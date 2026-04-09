69fb1e7090feca081569a196fcaa371e276c6ae1
Vignesh <vignesh-ext@captureiq.ai>
2026-04-09 09:05:08 +0530

    updated repo

--- examples/lead_example.py
+++ examples/lead_example.py
@@ -4,15 +4,17 @@
 import os
 from cms_python import CMSClient
 
-bearer_token = "your_bearer_token"
+bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTg3NTZiZGU3NTExNmU3YWMzZjVlNGQiLCJlbWFpbCI6InZpZ25lc2hyZWRkeTc5M0BnbWFpbC5jb20iLCJpYXQiOjE3NzMyMzI5ODUsImV4cCI6MTc4MTAwODk4NX0.22naMhoaQ19TkcB61kmvsXYj1kXEUrnCWoJvt-e55Pc"
 
-client = CMSClient(token=bearer_token)
+# Create client
+client = CMSClient(token=bearer_token, host="http://localhost:5000")
 
 # Standard lead
 response = client.track_lead(
-    click_id="5e6e1650-5641-4a2b-bec6-218288a86dbf",
+    click_id="8ea29346-1ef3-4dca-b472-1a1086998738",
+    mode="deferred",
     event_name="signup_started",
-    customer_external_id="user_42",
+    customer_external_id="user_999",
     customer_name="cutmeshort",
     customer_email="cutmeshort@cms.com",
 )
@@ -20,21 +22,21 @@ response = client.track_lead(
 print(response)
 
 # Deferred setup
-response = client.track_lead(
-    click_id="5e6e1650-5641-4a2b-bec6-218288a86dbf",
-    event_name="lead_captured",
-    customer_external_id="user_72",
-    customer_name="cutmeshort",
-    customer_email="cutmeshort@cms.com",
-    mode="deferred",
-)
+# response = client.track_lead(
+#     click_id="5e6e1650-5641-4a2b-bec6-218288a86dbf",
+#     event_name="lead_captured",
+#     customer_external_id="user_72",
+#     customer_name="cutmeshort",
+#     customer_email="cutmeshort@cms.com",
+#     mode="deferred",
+# )
 
-print(response)
+# print(response)
 
-# Follow-up event
-response = client.track_lead(
-    event_name="kyc_completed",
-    customer_external_id="user_72",
-)
+# # Follow-up event
+# response = client.track_lead(
+#     event_name="kyc_completed",
+#     customer_external_id="user_72",
+# )
 
-print(response)
+# print(response)
