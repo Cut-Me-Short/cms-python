c20bb5226322a6b7b1726aaa055ee23769763b4e
Vignesh <vignesh-ext@captureiq.ai>
2026-04-09 09:07:32 +0530

    updated examples

--- examples/lead_example.py
+++ examples/lead_example.py
@@ -4,14 +4,14 @@
 import os
 from cms_python import CMSClient
 
-bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTg3NTZiZGU3NTExNmU3YWMzZjVlNGQiLCJlbWFpbCI6InZpZ25lc2hyZWRkeTc5M0BnbWFpbC5jb20iLCJpYXQiOjE3NzMyMzI5ODUsImV4cCI6MTc4MTAwODk4NX0.22naMhoaQ19TkcB61kmvsXYj1kXEUrnCWoJvt-e55Pc"
+bearer_token = "your_bearer_token_here"
 
 # Create client
-client = CMSClient(token=bearer_token, host="http://localhost:5000")
+client = CMSClient(token=bearer_token)
 
 # Standard lead
 response = client.track_lead(
-    click_id="8ea29346-1ef3-4dca-b472-1a1086998738",
+    click_id="click_id",
     mode="deferred",
     event_name="signup_started",
     customer_external_id="user_999",
--- examples/sale_example.py
+++ examples/sale_example.py
@@ -5,15 +5,14 @@ import os
 from cms_python import CMSClient
 
 # Get Bearer token from environment
-bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2OTg3NTZiZGU3NTExNmU3YWMzZjVlNGQiLCJlbWFpbCI6InZpZ25lc2hyZWRkeTc5M0BnbWFpbC5jb20iLCJpYXQiOjE3NzMyMzI5ODUsImV4cCI6MTc4MTAwODk4NX0.22naMhoaQ19TkcB61kmvsXYj1kXEUrnCWoJvt-e55Pc"
+bbearer_token = "your_bearer_token_here"
 
 # Create client
-client = CMSClient(token=bearer_token, host="http://localhost:5000")
-# For custom base URL: client = CMSClient(token=bearer_token)
+client = CMSClient(token=bearer_token)
 
-    # Track a sale
+# Track a sale
 response = client.track_sale(
-    click_id="8ea29346-1ef3-4dca-b472-1a1086998738",
+    click_id="click_id",
     event_name="purchase_completed",
     customer_external_id="user_42",
     invoice_id="inv_987",
